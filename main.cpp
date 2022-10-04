4d90a99fdb09e3fa0d8a642db83aba7a2c6b9d40
ElizabethZim <90911446+ElizabethZim@users.noreply.github.com>
2022-10-04 23:02:18 +0300

    fdf

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Go to commits..." << std::endl;
+<<<<<<< HEAD
     std::cout << "Commit from PC" << std::endl;
+=======
+    std::cout << "Commit from Github..." << std::endl;
+>>>>>>> 4b478b3712cd18ea2adcc3086d60c07a5b7d7580
     return 0;
 }
