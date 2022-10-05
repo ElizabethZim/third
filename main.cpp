43e20c4f1ec4a0f79ded7a4360157919a3eca7ec
ElizabethZim <90911446+ElizabethZim@users.noreply.github.com>
2022-10-05 21:06:52 +0300

    new branch

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Go to commits..." << std::endl;
-<<<<<<< HEAD
     std::cout << "Commit from PC" << std::endl;
-=======
     std::cout << "Commit from Github..." << std::endl;
->>>>>>> 4b478b3712cd18ea2adcc3086d60c07a5b7d7580
+
+    int y = 0;
+    y += 1;
+    std::cout << y;
     return 0;
 }
